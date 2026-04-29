f1885281a8b01e269ac6b18b9ec19978d99b9118
LightBlueJacketStudio <lightbluejacketstudio@gmail.com>
2026-04-29 11:52:53 -0700

    replaced old model with a new one with batch normalization

--- customized_pipeline/model.py
+++ customized_pipeline/model.py
@@ -1,33 +1,39 @@
 import torch
 import torch.nn as nn
 
-class CatDogCNN(nn.Module):
+class DogCatCNN(nn.Module):
     def __init__(self):
         super().__init__()
-
         self.features = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
+            nn.Conv2d(3, 32, kernel_size=3, padding=1), # Convolutional layer with 32 filters
+            nn.BatchNorm2d(32), # Batch normalization for faster convergence
+            nn.ReLU(inplace=True), # ReLU activation for non-linearity
+            nn.MaxPool2d(2, 2), # Max pooling to reduce spatial dimensions
 
             nn.Conv2d(32, 64, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
 
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
-        )
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
 
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
+        )
         self.classifier = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
             nn.Flatten(),
-            nn.Linear(128 * 28 * 28, 256),   # for 224x224 input
-            nn.ReLU(),
+            nn.Linear(256, 128),
+            nn.ReLU(inplace=True),
             nn.Dropout(0.5),
-            nn.Linear(256, 1),               # binary output
+            nn.Linear(128, 1),
         )
 
     def forward(self, x):
         x = self.features(x)
-        x = self.classifier(x)
-        return x
+        return self.classifier(x)
